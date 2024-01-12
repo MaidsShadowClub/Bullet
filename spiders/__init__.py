b4da53639c6e6458297907e9f1441b3c8d0d627b
ReKreker <ReKreker@UwU.com>
2024-01-13 00:02:36 +0300

    Update importing spiders

--- spiders/__init__.py
+++ spiders/__init__.py
@@ -2,3 +2,8 @@
 #
 # Please refer to the documentation for information on how to create and manage
 # your spiders.
+from spiders.AndroidCVE import AndroidCVEScraper
+from spiders.GPZArticles import GPZArticlesScraper
+from spiders.HuaweiCVE import HuaweiCVEScraper
+from spiders.LgCVE import LgCVEScraper
+from spiders.SamsungCVE import SamsungCVEScraper
